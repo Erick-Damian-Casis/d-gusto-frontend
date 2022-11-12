d584565fe01d963ac79de9c07aafcf07136ba748
1727003855 <ejc.damian@yavirac.edu.ec>
2022-11-11 19:47:20 -0500

    fix services

---
+++ src/services/AuthService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const URL_API='http://127.0.0.1:8000/api/v1/public/';
+
+export const login=(data)=>{
+    return axios.post(URL_API+'login', data)
+        .then(response=>{
+            console.log(response)
+        }).catch(error=>{
+            console.log(error)
+        })
+}
+
+export const register=(data)=>{
+    return axios.post(URL_API+'register', data)
+        .then(response=>{
+            console.log(response)
+        }).catch(error=>{
+            console.log(error)
+        })
+}
--- src/services/OrderServices.js
+++ src/services/OrderServices.js
@@ -1,5 +1,41 @@
 import axios from "axios";
 
+const API_URL = 'http://127.0.0.1:8000/api/v1/private/orders/';
+
 export const getOrders=()=>{
-    return axios()
+    return axios(API_URL)
+        .then(response=>{
+            return response.data.data;
+        }).catch(error=>{
+            console.log(error);
+        })
+}
+
+export const getOrder=(id)=>{
+    return axios(API_URL+id)
+        .then(response=>{
+            return response.data.data
+        }).catch(error=>{
+            console.log(error);
+        })
+}
+
+export const createOrder=(order)=>{
+    return axios.post(API_URL, order)
+        .then(response=>{
+            console.log(response.data)
+        }).catch(
+            error=>{
+                console.log(error)
+            });
+}
+
+export const destroyOrder =(id)=>{
+    return axios.delete(API_URL+id)
+        .then(response=>{
+            console.log(response.data);
+        }).catch(
+            error=>{
+                console.log(error)
+            });
 }
